3e6deb4297918b8b568a3ed378a8e414ddf49514
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-11-02 21:16:58 -0500

    Add OrderForm component

--- app/ui/Foundation/App.tsx
+++ app/ui/Foundation/App.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import "@shopify/polaris/dist/styles.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
-import { AppProvider, Page, Card, Button } from "@shopify/polaris";
+import { AppProvider, Page } from "@shopify/polaris";
+import { OrderForm } from "ui/OrderForm";
 
 export default function App() {
   return (
     <AppProvider i18n={enTranslations}>
-      <Page title="Example app">
-        <Card sectioned>
-          <Button onClick={() => alert("Button clicked!")}>
-            Example button
-          </Button>
-        </Card>
+      <Page title="Billify">
+        <OrderForm />
       </Page>
     </AppProvider>
   );
