7b2a7a0fb4b397c9a22b5c73e215e7bb4b84c163
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-11-07 16:51:09 -0500

    Add apollo

--- app/ui/Foundation/App.tsx
+++ app/ui/Foundation/App.tsx
@@ -4,12 +4,21 @@ import enTranslations from "@shopify/polaris/locales/en.json";
 import { AppProvider, Page } from "@shopify/polaris";
 import { OrderForm } from "ui/OrderForm";
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <AppProvider i18n={enTranslations}>
-      <Page title="Billify">
-        <OrderForm />
-      </Page>
-    </AppProvider>
+    <ApolloProvider client={client}>
+      <AppProvider i18n={enTranslations}>
+        <Page title="Billify">
+          <OrderForm />
+        </Page>
+      </AppProvider>
+    </ApolloProvider>
   );
 }
